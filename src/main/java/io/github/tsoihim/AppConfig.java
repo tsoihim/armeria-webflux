4d15df00a5fa13e941523bc81f4310ee37db3e13
narachoi <nara.choi@samsung.com>
2026-01-06 16:18:41 +0900

    Apply armeria

--- src/main/java/io/github/tsoihim/AppConfig.java
+++ src/main/java/io/github/tsoihim/AppConfig.java
@@ -1,10 +1,43 @@
 package io.github.tsoihim;
 
-import lombok.RequiredArgsConstructor;
+import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.client.circuitbreaker.CircuitBreakerClient;
+import com.linecorp.armeria.client.circuitbreaker.CircuitBreakerRule;
+import com.linecorp.armeria.server.docs.DocService;
+import com.linecorp.armeria.server.logging.AccessLogWriter;
+import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+import com.linecorp.armeria.spring.web.reactive.ArmeriaClientConfigurator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class AppConfig {
+    @Bean
+    public ArmeriaServerConfigurator armeriaServerConfigurator() {
+        return builder -> {
+            builder.serviceUnder("/docs", new DocService());
+            builder.decorator(LoggingService.newDecorator());
+            builder.accessLogWriter(AccessLogWriter.combined(), false);
+        };
+    }
 
+    @Bean
+    public ClientFactory clientFactory() {
+        return ClientFactory.insecure();
+    }
+
+    @Bean
+    public ArmeriaClientConfigurator armeriaClientConfigurator(ClientFactory clientFactory) {
+        return builder -> {
+            final CircuitBreakerRule rule = CircuitBreakerRule.builder()
+                    .onServerErrorStatus()
+                    .onException()
+                    .thenFailure();
+            builder.decorator(CircuitBreakerClient.builder(rule)
+                    .newDecorator());
+
+            builder.factory(clientFactory);
+        };
+    }
 }
